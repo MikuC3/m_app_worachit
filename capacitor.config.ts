92b334f8dc41aecb34deec94eae6aca3c370837b
Worachit Toto <Worachit8112@gmail.com>
2022-03-07 10:13:24 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'm_app_worachit',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
